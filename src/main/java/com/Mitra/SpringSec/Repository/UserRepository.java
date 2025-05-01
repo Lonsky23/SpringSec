8b4f8060b5747a8ff3794bd86361877d9f926ad5
Lonsky23 <mitramarlon2003@gmail.com>
2025-05-01 21:09:00 +0800

    Final Working Code

---
+++ src/main/java/com/Mitra/SpringSec/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Mitra.SpringSec.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Mitra.SpringSec.user.User;
+
+public interface UserRepository extends JpaRepository< User , Integer> {
+    
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/Mitra/SpringSec/user/Role.java
@@ -0,0 +1,6 @@
+package com.Mitra.SpringSec.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
